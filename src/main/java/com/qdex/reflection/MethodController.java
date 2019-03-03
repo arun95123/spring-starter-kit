7013f45b79c224c3ed8aa155dd54585a08007558
Venky <ezio123arun@gmail.com>
2019-03-03 19:52:12 +0530

    WIP implement generated controller

--- src/main/java/com/qdex/reflection/MethodController.java
+++ src/main/java/com/qdex/reflection/MethodController.java
@@ -2,9 +2,10 @@ package com.qdex.reflection;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.qdex.reflection.api.MethodsApi;
 
 @RestController
-public class controller {
+public class MethodController implements MethodApi{
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
